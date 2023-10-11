375b8c81c4e2c9c827bab2ada91e788efeee8374
김우빈 <kwb020312@naver.com>
2023-10-11 22:21:00 +0900

    Feat: Scraping setup

---
+++ lib/scraper/index.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (100000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+
+  try {
+    const response = await axios.get(url, options);
+  } catch (error: any) {
+    throw new Error(`failed to scrape product: ${error.message}`);
+  }
+}
