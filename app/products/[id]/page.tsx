7c829d3844e9fedfa3435f294cef4115ee701bcc
김우빈 <kwb020312@naver.com>
2023-10-14 23:01:44 +0900

    Feat: Email Funcionality using Nodemails

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
 import { getProductById, getSimilarProducts } from "@/lib/actions";
@@ -153,7 +154,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
               />
             </div>
           </div>
-          Modal
+          <Modal productId={id} />
         </div>
       </div>
 
