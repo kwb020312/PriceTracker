34eb3083b046ff4e4d3c69d1d0a3839b33525860
김우빈 <kwb020312@naver.com>
2023-10-15 15:18:00 +0900

    Fix: Deploy error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,10 @@ import {
 } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -38,7 +42,7 @@ export async function GET() {
         };
 
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: scrapedProduct.url },
+          { url: product.url },
           product
         );
 
